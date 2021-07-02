bc7738fb9f5b45a15590d6a3e719760201b292bc
Klermint <74746572+Klermint@users.noreply.github.com>
2021-07-02 23:36:25 +0930

    Update main.py

--- opencvtest/main.py
+++ opencvtest/main.py
@@ -1,32 +1,5 @@
 import cv2
-import numpy as np
 
-#img = cv2.imread("image.jpg")
-#cv2.imshow('input_image', img)
-"""
-# playing with perspective
-width, height = 1250, 577
-c1= [100,100]
-c2= [287,188]
-c3= [154,482]
-c4= [352,440]
-
-pts1 = np.float32([c1,c2,c3,c4])
-pts2 = np.float32([[0,0],[width,0],[0,height],[width,height]])
-matrix = cv2.getPerspectiveTransform(pts1,pts2)
-imgOut = cv2.warpPerspective(img, matrix,(width,height))
-
-cv2.imshow('',imgOut)
-"""
-"""
-# edge detection
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-blur = cv2.GaussianBlur(gray,(7,7),1)
-canny = cv2.Canny(blur,50,50)
-
-cv2.imshow('canny', canny)
-"""
-# """
 # face detection
 # load the cascade
 face_cascade = cv2.CascadeClassifier("haarcascades/haarcascade_frontalface_default.xml")
@@ -51,5 +24,3 @@ while True:
         break
 cap.release()
 cv2.destroyAllWindows()
-# """
-
